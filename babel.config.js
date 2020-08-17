e5fd56c69129a010d788e6f4e999d2a8eb408f59
André Cruz <andremiguelcruz@msn.com>
2020-08-17 14:14:50 +0100

    chore: improve babel.config.js ignore

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = (api) => {
     api.cache(true);
 
     return {
-        ignore: process.env.NODE_ENV === 'test' ? [] : ['**/*.test.js', '**/__snapshots__'],
+        ignore: process.env.BABEL_ENV ? ['**/*.test.js', '**/__snapshots__', '**/__mocks__', '**/__fixtures__'] : [],
         presets: [
             ['@moxy/babel-preset/lib', { react: true }],
         ],
